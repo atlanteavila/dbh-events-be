4b65fd3584eed2228c96a1e530ff749f01a26b47
Atlante Avila <atlanteavila@gmail.com>
2022-10-03 15:43:03 -0400

    Removes console logs

--- src/objection.js
+++ src/objection.js
@@ -5,8 +5,6 @@ module.exports = function (app) {
   const { client, connection } = app.get('postgres');
   connection.ssl = { rejectUnauthorized: false };
   const db = knex({ client, connection, useNullAsDefault: false });
-  console.log('db', db)
-  console.log('db', connection)
   Model.knex(db);
 
   app.set('knex', db);
