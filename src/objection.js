eb1fed0e20d0e766074aabfdb62e0f99002a6e93
Atlante Avila <atlanteavila@gmail.com>
2022-10-03 15:41:20 -0400

    objection test

--- src/objection.js
+++ src/objection.js
@@ -3,8 +3,10 @@ const knex = require('knex');
 
 module.exports = function (app) {
   const { client, connection } = app.get('postgres');
+  connection.ssl = { rejectUnauthorized: false };
   const db = knex({ client, connection, useNullAsDefault: false });
-
+  console.log('db', db)
+  console.log('db', connection)
   Model.knex(db);
 
   app.set('knex', db);
